b484872ab1c2d7097fa66c6fec74d3c1748b7fbe
Mathieu Benoit <mathben963@gmail.com>
2018-11-05 20:13:59 -0500

    Add model raw material lines to separate from parent model. - add model log lines to show log

--- models/indexation_raw_material.py
+++ models/indexation_raw_material.py
@@ -6,6 +6,18 @@ from odoo import api, fields, tools, models, _
 class IndexationRawMaterial(models.Model):
     _name = 'indexation.raw_material'
     _description = 'Indexation data about raw material'
+
+
+class IndexationRawMaterialLogLines(models.Model):
+    _name = 'indexation.raw_material.log.lines'
+    _description = 'Log and error about indexation raw material algorithm execution.'
+
+    message = fields.Text()
+
+
+class IndexationRawMaterialLines(models.Model):
+    _name = 'indexation.raw_material.lines'
+    _description = 'Lines of each value of indexation data about raw material'
     # _order = "purchase_order_id"
 
     purchase_id = fields.Many2one('purchase.order', 'Purchase Order')
